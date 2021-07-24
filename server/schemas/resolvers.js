6e66334cf523f9fae34bf2fe785641af34cf4994
Maria Donaji Velazquez <xunaxidonaji@hotmail.com>
2021-07-23 17:10:08 -0700

    Adding resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,30 +1,51 @@
-const { User, Thought } = require("../models");
+const { User, Book } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    thoughts: async (parent, { username }) => {
+    books: async (parent, { username }) => {
       const params = username ? { username } : {};
-      return Thought.find(params).sort({ createdAt: -1 });
+      return Book.find(params);
     },
-    // place this inside of the `Query` nested object right after `thoughts`
-    thought: async (parent, { _id }) => {
-      return Thought.findOne({ _id });
+    // place this inside of the `Query` nested object right after `books`
+    book: async (parent, { _id }) => {
+      return Book.findOne({ _id });
     },
     // get all users
     users: async () => {
-      return User.find()
-        .select("-__v -password")
-        .populate("friends")
-        .populate("thoughts");
+      return User.find().select("-__v -password").populate("books");
     },
     // get a user by username
     user: async (parent, { username }) => {
       return User.findOne({ username })
         .select("-__v -password")
-        .populate("friends")
-        .populate("thoughts");
+        .populate("books");
     },
   },
+  Mutation: {
+    addUser: async (parent, args) => {
+        const user = await User.create(args);
+        const token = signToken(user);
+      
+        return { token, user };
+      },
+    login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+      
+        if (!user) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+      
+        const correctPw = await user.isCorrectPassword(password);
+      
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+      
+        const token = signToken(user);
+        return { token, user };
+      }
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,36 +2,39 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  type Thought {
-    _id: ID
-    thoughtText: String
-    createdAt: String
-    username: String
-    reactionCount: Int
-    reactions: [Reaction]
-  }
-
-  type Reaction {
-    _id: ID
-    reactionBody: String
-    createdAt: String
-    username: String
+  type Book {
+    _Id: ID
+    bookId: String
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String
   }
 
   type User {
     _id: ID
     username: String
     email: String
-    friendCount: Int
-    thoughts: [Thought]
-    friends: [User]
+    password: String
+    savedBooks: [Book]
   }
 
   type Query {
     users: [User]
     user(username: String!): User
-    thoughts(username: String): [Thought]
-    thought(_id: ID!): Thought
+    books(username: String): [Book]
+    book(_id: ID!): Book
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 `;
 
